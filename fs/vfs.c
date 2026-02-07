8ec8962fcbfa8ccd5d8358656b6242fc99a62def
imbluedabr <baldergroen@gmail.com>
2026-02-07 18:02:17 +0100

    added more features to the vfs

--- fs/vfs.c
+++ fs/vfs.c
@@ -7,11 +7,16 @@ struct superblock* rootfs;
 void walk_path_init(path_walk_t* state, const char* path)
 {
     memset(state->path_cpy, 0, FS_PATH_LEN);
+    if (*path == '/') { //absolute path
+        path++;
+    } else {
+        //relative paths not suported yet
+    }
     strncpy(state->path_cpy, (char*) path, FS_PATH_LEN);
     state->fs_state.fname = state->path_cpy;
     state->path_ptr = state->path_cpy;
     state->fs_state.dir = NULL;
-    state->fs_state.fs = rootfs; //we start th search at the root
+    state->fs_state.fs = rootfs; //we start the search at the root
 }
 
 
@@ -55,5 +60,50 @@ int mount_root(const char* fs_name, dev_t devno)
     rootfs = fs->mount(dev, NULL);
     return 0;
 }
+
+int8_t mount_init(vfs_mount_t* state, const char* path, const char* fs_name, dev_t devno)
+{
+    state->fs = lookup_filesystem(fs_name);
+    if (state->fs == NULL) {
+        return -1;
+    }
+    state->dev = device_lookup(devno);
+    if (state->dev == NULL) {
+        return -1;
+    }
+    walk_path_init(&state->path, path);
+    return 0;
+}
+
+int8_t mount_update(vfs_mount_t* state)
+{
+    int8_t stat = walk_path(&state->path);
+    if (stat < 0) {
+        return stat;
+    }
+    if (stat == 1) {
+        struct inode* i = state->path.fs_state.dir;
+        i->fs = state->fs->mount(state->dev, NULL);
+        i->mode = FS_MODE_MOUNT;
+        return 1;
+    }
+    return 0;
+}
+
+
+void vfs_close(struct fd* descriptor)
+{
+    struct inode* i = descriptor->file;
+    if (i == NULL || i->refcount == 0) {
+        return;
+    }
+
+    if (--i->refcount == 0) {
+        free_inode(i);
+    }
+
+    descriptor->file = NULL;
+}
+
 
 
--- include/fs/vfs.h
+++ include/fs/vfs.h
@@ -8,20 +8,18 @@ typedef struct {
     fs_lookup_t fs_state;
 } path_walk_t;
 
+typedef struct {
+    path_walk_t path;
+    struct super_ops* fs;
+    struct device* dev;
+} vfs_mount_t;
+
 void walk_path_init(path_walk_t* state, const char* path);
 int8_t walk_path(path_walk_t* state);
 
 int mount_root(const char* fs_name, dev_t devno);
+int8_t mount_init(vfs_mount_t* state, const char* path, const char* fs_name, dev_t devno);
+int8_t mount_update(vfs_mount_t* state);
+void vfs_close(struct fd* decsriptor);
 
-int vfs_write(int fd, const void* buffer, uint32_t count);
-int vfs_lstat(int fd, struct stat* statbuff);
-int vfs_lseek(int fd, uint32_t offset, int whence);
-void vfs_close(int fd);
-int vfs_readdir(int fd, void* buffer, uint32_t count);
-int vfs_pread(int fd, void* buffer, uint32_t count, uint32_t offset);
-int vfs_pwrite(int fd, const void* buffer, uint32_t count, uint32_t offset);
-void vfs_mount(const char* mount, const char* dev_path, const char* fs_name);
-void vfs_umount(const char* mount);
-int vfs_mkdir(const char* path);
-int vfs_rmdir(const char* path);
-int vfs_unlink(const char* path);
+
